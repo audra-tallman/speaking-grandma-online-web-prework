c78644744e123753a822dacfdb0fb135007266be
Audra Tallman <audra.tallman@hotmail.com>
2019-02-20 19:16:15 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -8,7 +8,7 @@ def speak_to_grandma(phrase)
     return "NO, NOT SINCE 1938!"
   elsif phrase.downcase()
     return "HUH?! SPEAK UP, SONNY!" 
-  elsif phrase.downcase()
+  else phrase.downcase()
     return "HUH?! SPEAK UP, SONNY!" 
   end
 end
