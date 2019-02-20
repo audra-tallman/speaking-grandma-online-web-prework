74ca4692ec0effe49c49970a0d6625000649f5ef
Audra Tallman <audra.tallman@hotmail.com>
2019-02-20 19:26:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,9 +3,7 @@ def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
   end
-  elsif phrase.upcase()
-    return "NO, NOT SINCE 1938!"
-  elsif phrase.upcase()
+  if phrase.upcase()
     return "NO, NOT SINCE 1938!"
   elsif phrase != phrase.upcase
     return "HUH?! SPEAK UP, SONNY!" 
